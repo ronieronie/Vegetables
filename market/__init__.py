5e063f2143c0942c58e3378a91e50ae6110b3a87
ronieronie <cardoza.ronie.r@gmail.com>
2025-08-11 14:10:37 +0800

    Update __init__.py
    asd

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 import os
-from sqlalchemy import create_engine
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 # app.config['SECRET_KEY'] = '1e32f81eb715404d20b202fd'
@@ -18,38 +17,25 @@ app = Flask(__name__)
 
 #localhost
 # app.secret_key = 'supersecretkey'  # or use os.getenv('SECRET_KEY')
-username = os.getenv('MYSQLUSER')
-password = os.getenv('MYSQLPASSWORD')
-host = os.getenv('MYSQLHOST')
-port = os.getenv('MYSQLPORT')  # Railway port from your URL
-database = os.getenv('MYSQLDATABASE')
+# username = os.getenv('MYSQLUSER', 'root')
+# password = os.getenv('MYSQLPASSWORD', 'hElzjxVLpYsPkaiulbRTVpFQgHTvgaXz')
+# host = os.getenv('MYSQLHOST', 'interchange.proxy.rlwy.net')
+# port = os.getenv('MYSQLPORT', '15150')  # Railway port from your URL
+# database = os.getenv('MYSQLDATABASE', 'railway')
 
 app.secret_key = os.getenv('SECRET_KEY')
-# username = os.getenv('MYSQLUSER')
-# password = os.getenv('MYSQLPASSWORD')
-# host = os.getenv('MYSQLHOST')
-# port = os.getenv('MYSQLPORT')  # Railway port from your URL
-# database = os.getenv('MYSQLDATABASE')
+env = os.getenv("RAILWAY_ENV", "development")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+host = os.getenv("MYSQLHOST") or ("mysql.railway.internal" if env == "production" else "interchange.proxy.rlwy.net")
+port = os.getenv("MYSQLPORT") or ("3306" if env == "production" else "15150")
+username = os.getenv("MYSQLUSER", "root")
+password = os.getenv("MYSQLPASSWORD", "hElzjxVLpYsPkaiulbRTVpFQgHTvgaXz")
+database = os.getenv("MYSQLDATABASE", "railway")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-# username = os.getenv('MYSQLUSER')
-# password = os.getenv('MYSQLPASSWORD')
-# host = os.getenv('MYSQLHOST')
-# port = os.getenv('MYSQLPORT')
-# database = os.getenv('MYSQLDATABASE')
-
-# uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
-# engine = create_engine(uri, pool_pre_ping=True)
-
-# try:
-#     with engine.connect() as conn:
-#         result = conn.execute("SELECT 1")
-#         print("✅ Connected to MySQL:", result.scalar())
-# except Exception as e:
-#     print("Connection failed:", e)
 
 
 db.init_app(app)
