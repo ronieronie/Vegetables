68f899a3eb651dfab1365267ea8cc71c10d1ff04
ronieronie <cardoza.ronie.r@gmail.com>
2025-08-11 10:26:59 +0800

    asd
    asd

--- market/__init__.py
+++ market/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # login_manager.login_message_category = "info"
 
 
-app.secret_key = 'supersecretkey'
+app.secret_key = os.getenv('SECRET_KEY', 'supersecretkey')
 
 
 username = os.getenv('MYSQLUSER', 'root')
