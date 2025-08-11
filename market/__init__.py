5ab1d6bba2b5a5c8dc829aef881d66bcfb827ecd
ronieronie <cardoza.ronie.r@gmail.com>
2025-08-11 14:17:41 +0800

    Update __init__.py
    asd

--- market/__init__.py
+++ market/__init__.py
@@ -23,18 +23,14 @@ app = Flask(__name__)
 # port = os.getenv('MYSQLPORT', '15150')  # Railway port from your URL
 # database = os.getenv('MYSQLDATABASE', 'railway')
 
-app.secret_key = os.getenv('SECRET_KEY')
-env = os.getenv("RAILWAY_ENV", "development")
+app.secret_key = os.getenv('SECRET_KEY', 'supersecretkey')
+username = os.getenv('MYSQLUSER', 'root')
+password = os.getenv('MYSQLPASSWORD', 'hElzjxVLpYsPkaiulbRTVpFQgHTvgaXz')
+host = os.getenv('MYSQLHOST', 'mysql.railway.internal')
+port = os.getenv('MYSQLPORT', 3306)  # Railway port from your URL
+database = os.getenv('MYSQLDATABASE')
 
-host = os.getenv("MYSQLHOST") or ("mysql.railway.internal" if env == "production" else "interchange.proxy.rlwy.net")
-port = os.getenv("MYSQLPORT") or ("3306" if env == "production" else "15150")
-username = os.getenv("MYSQLUSER", "root")
-password = os.getenv("MYSQLPASSWORD", "hElzjxVLpYsPkaiulbRTVpFQgHTvgaXz")
-database = os.getenv("MYSQLDATABASE", "railway")
-
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
